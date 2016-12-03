5d96c6441e29b98e0eda93333a36b0fd4964041f
Jaeho Cho <real0131@gmail.com>
2016-12-03 23:20:07 +0900

    add web-03

---
+++ web/03.php
@@ -0,0 +1,26 @@
+﻿<?php
+	header('Content-Type: application/json');
+	if ($_GET['param1'] == null and $_GET['param2']==null){
+		$arr = array ('error'=> true);
+		urlencode($arr);
+		echo json_encode($arr);	
+	}else if($_GET['param1'] == null){
+		$arr = array ('error'=> true);
+		urlencode($arr);
+		echo json_encode($arr);
+	}else if($_GET['param2'] == null){
+		$arr = array ('error'=> true);
+		urlencode($arr);
+		echo json_encode($arr);
+	}else{
+		if( $_GET['param1'] == $_GET['param2']){
+			$arr = array ('error'=> false,'result'=> true);
+			urlencode($arr);
+			echo json_encode($arr);
+		}else{
+			$arr = array ('error'=> false,'result'=> false);
+			urlencode($arr);
+			echo json_encode($arr);
+		}
+	}
+?>
